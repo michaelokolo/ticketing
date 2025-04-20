ff2b52fb0cc233ccac8a99c75d8097b07cfc3678
Mike <michaelokolo62@yahoo.com>
2025-04-20 16:30:57 +0100

    customizing error handler

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,14 +1,27 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction, ErrorRequestHandler } from 'express';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
-export const errorHandler = (
-  err: Error,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  console.log('Something went wrong', err);
+export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  if (err instanceof RequestValidationError) {
+    const formattedErrors = err.errors
+      .filter((error) => error.type === 'field')
+      .map((error) => {
+        return { message: error.msg, field: error.path };
+      });
+    res.status(400).send({ errors: formattedErrors });
+    return;
+  }
+
+  if (err instanceof DatabaseConnectionError) {
+    res.status(500).send({
+      errors: [{ message: err.reason }],
+    });
+    return;
+  }
 
   res.status(400).send({
-    message: err.message,
+    errors: [{ message: 'Something went wrong' }],
   });
+  return;
 };

ae6946b06595736acd545a0f9567fddd39213cef
Mike <michaelokolo62@yahoo.com>
2025-04-20 13:36:40 +0100

    updated the auth route  with express validator

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,30 @@
-import express from 'express';
+import express, { Request, Response, RequestHandler } from 'express';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/users/signup', (req, res) => {
-  res.send('Hi there!');
-});
+router.post(
+  '/api/users/signup',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Password must be between 4 and 20 characters'),
+  ],
+  (req: Request, res: Response): void => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.status(400).send(errors.array());
+      return;
+    }
+    const { email, password } = req.body;
+
+    console.log('Creating a user...');
+
+    res.send({});
+  }
+);
 
 export { router as signupRouter };

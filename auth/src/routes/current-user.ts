82eb5fcff14877b2b06665d88c145142b0cf34fc
Mike <michaelokolo62@yahoo.com>
2025-04-24 00:03:33 +0100

    set up the current user route handler

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,21 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import { Request, Response } from 'express';
 
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there!');
+  if (!req.session?.jwt) {
+    res.send({ currentUser: null });
+    return;
+  }
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.send({ currentUser: payload });
+  } catch (err) {
+    res.send({ currentUser: null });
+    return;
+  }
 });
 
 export { router as currentuserRouter };

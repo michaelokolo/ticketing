2780e1375df00ee1f3deb907ce5a1bcc93b4a0c9
Mike <michaelokolo62@yahoo.com>
2025-04-26 05:20:49 +0100

    set up test environment

---
+++ auth/src/app.ts
@@ -0,0 +1,34 @@
+import express, { Request, Response, NextFunction } from 'express';
+import { json } from 'body-parser';
+
+import cookieSession from 'cookie-session';
+
+import { currentuserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+
+app.use(currentuserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  next(new NotFoundError());
+});
+
+app.use(errorHandler);
+
+export { app };

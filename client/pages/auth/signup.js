eeef99bf7b750dd93fb68e925ed77504f251032d
Mike <michaelokolo62@yahoo.com>
2025-04-27 19:50:37 +0100

    intial setup of signup form and redirect

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,36 +1,54 @@
 import { useState } from 'react';
+import Router from 'next/router';
+import useRequest from '../../hooks/use-request';
 
 export default () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const { doRequest, errors } = useRequest({
+    url: '/api/users/signup',
+    method: 'post',
+    body: { email, password },
+    onSuccess: () => Router.push('/'),
+  });
 
-  const onSubmit = (event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
 
-    console.log(email, password);
+    await doRequest();
   };
 
   return (
-    <form onSubmit={onSubmit}>
-      <h1>Sign Up</h1>
-      <div className="form-group">
-        <label>Email Address</label>
-        <input
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          className="form-control"
-        />
+    <form
+      onSubmit={onSubmit}
+      className="container d-flex justify-content-center mt-5"
+    >
+      <div className="w-100" style={{ maxWidth: '400px' }}>
+        <div className="border shadow-lg p-4 rounded bg-white">
+          <h1 className="text-center mb-4">Sign Up</h1>
+          <div className="form-group">
+            <label>Email Address</label>
+            <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="form-control"
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Password</label>
+            <input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              className="form-control"
+            />
+          </div>
+          {errors}
+          <button type="submit" className="btn btn-primary mt-4 w-100">
+            Sign Up
+          </button>
+        </div>
       </div>
-      <div className="form-group">
-        <label>Password</label>
-        <input
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          type="password"
-          className="form-control"
-        />
-      </div>
-      <button className="btn btn-primary">Sign Up</button>
     </form>
   );
 };

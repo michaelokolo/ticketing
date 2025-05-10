0271b9cbb715c041f5e7a779fdf6093665895976
Mike <michaelokolo62@yahoo.com>
2025-05-10 11:36:55 +0100

    Initial set up for the orders services

---
+++ orders/src/app.ts
@@ -0,0 +1,36 @@
+import express, { Request, Response, NextFunction } from 'express';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import {
+  errorHandler,
+  NotFoundError,
+  currentUser,
+} from '@mokolotickets/common';
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+app.use(currentUser);
+
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  next(new NotFoundError());
+});
+
+app.use(errorHandler);
+
+export { app };

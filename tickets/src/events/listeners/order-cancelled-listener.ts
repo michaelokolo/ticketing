4ba9d62cad5c29757e6484cae74ebb5f5419258d
Mike <michaelokolo62@yahoo.com>
2025-05-21 21:22:59 +0100

    implemented order cancelled event listener

--- tickets/src/events/listeners/order-cancelled-listener.ts
+++ tickets/src/events/listeners/order-cancelled-listener.ts
@@ -1,36 +1,32 @@
 import { Message } from 'node-nats-streaming';
-import { Listener, OrderCreatedEvent, Subjects } from '@mokolotickets/common';
+import { Listener, OrderCancelledEvent, Subjects } from '@mokolotickets/common';
 import { queueGroupName } from './queue-group-name';
 import { Ticket } from '../../models/ticket';
 import { TicketUpdatedPublisher } from '../publishers/ticket-updated-publisher';
 
-export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
-  readonly subject = Subjects.OrderCreated;
+export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
+  readonly subject = Subjects.OrderCancelled;
   queueGroupName = queueGroupName;
 
-  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
-    // Find the ticket that the order is reserving
+  async onMessage(data: OrderCancelledEvent['data'], msg: Message) {
     const ticket = await Ticket.findById(data.ticket.id);
 
-    // If no ticket, throw error
     if (!ticket) {
       throw new Error('Ticket not found');
     }
 
-    // Mark the ticket as being reserved by setting its orderId property
-    ticket.set({ orderId: data.id });
-
-    // Save the ticket
+    ticket.set({ orderId: undefined });
     await ticket.save();
+
     await new TicketUpdatedPublisher(this.client).publish({
       id: ticket.id,
-      price: ticket.price,
       title: ticket.title,
+      price: ticket.price,
       userId: ticket.userId,
-      orderId: ticket.orderId,
       version: ticket.version,
+      orderId: ticket.orderId,
     });
-    // ack the message
+
     msg.ack();
   }
 }

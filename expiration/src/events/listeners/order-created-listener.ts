2b8df35ab909a75e4929eafbb3a1daba2819c6f7
Mike <michaelokolo62@yahoo.com>
2025-05-22 22:56:35 +0100

    Set up the order created listener:expiration service

---
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,17 @@
+import { Message } from 'node-nats-streaming';
+import { Listener, OrderCreatedEvent, Subjects } from '@mokolotickets/common';
+import { queueGroupName } from './queue-group-name';
+import { expirationQueue } from '../../queues/expiration-queue';
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    await expirationQueue.add({
+      orderId: data.id,
+    });
+
+    msg.ack();
+  }
+}

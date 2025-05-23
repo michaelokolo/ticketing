87a53915f806433867984065d7b9f81d2123ca41
Mike <michaelokolo62@yahoo.com>
2025-05-24 00:04:43 +0100

    Initial set up for the payment service

---
+++ payments/src/app.ts
@@ -0,0 +1,27 @@
+import express, { Request, Response, NextFunction } from 'express';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import {
+  errorHandler,
+  NotFoundError,
+  currentUser,
+} from '@mokolotickets/common';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+app.use(currentUser);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  next(new NotFoundError());
+});
+
+app.use(errorHandler);
+
+export { app };

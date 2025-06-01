20407c2fa67a888390813919dac6e814169d545a
Mike <michaelokolo62@yahoo.com>
2025-06-01 20:23:37 +0100

    Last commit after coding

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import Router from 'next/router';
+import useRequest from '../../hooks/use-request';
 
 const OrderShow = ({ order, currentUser }) => {
   const [timeLeft, setTimeLeft] = useState(0);
@@ -9,7 +11,7 @@ const OrderShow = ({ order, currentUser }) => {
     body: {
       orderId: order.id,
     },
-    onSuccess: (payment) => console.log(payment),
+    onSuccess: (payment) => Router.push('/orders'),
   });
 
   useEffect(() => {
